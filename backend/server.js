72d501e0e7f59be076cdb2185faebf30cb87cf70
Ojas Mhatre <mhatreojas192@gmail.com>
2025-08-06 16:29:50 +0530

    changes in env

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,4 @@ require('dotenv').config()
 const app=require('./src/app')
 
 
-app.listen(PORT || 3000,()=>{console.log("Server started ....")}) 
+app.listen(process.env.PORT || 3000,()=>{console.log("Server started ....")}) 
