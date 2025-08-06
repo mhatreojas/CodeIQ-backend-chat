b8c280574165687776894f9729b60e1939207d18
Ojas Mhatre <mhatreojas192@gmail.com>
2025-08-06 16:29:11 +0530

    production changes

--- backend/server.js
+++ backend/server.js
@@ -2,4 +2,4 @@ require('dotenv').config()
 const app=require('./src/app')
 
 
-app.listen(3000,()=>{console.log("Server started ....")}) 
+app.listen(PORT || 3000,()=>{console.log("Server started ....")}) 
